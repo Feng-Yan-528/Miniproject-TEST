c8220fd6c3cf3c01fe4557b77b126eeae4dbd1f5
Feng-Yan-528 <ml19f2y@leeds.ac.uk>
2021-05-11 11:39:54 +0100

    Frequency division success
    Realized 1/3 frequency division

--- F_a.v
+++ F_a.v
@@ -13,27 +13,48 @@ module F_a(
  parameter	WIDTH	= 2;//The number of digits of the counter. The maximum value of the count is 2**WIDTH-1
  parameter	N	= 3; 
  
- reg  [WIDTH-1:0] cnt;//The register is used to store the value of the counter
- reg  j;
+ reg  [WIDTH-1:0] cnt_1,cnt_0;//The register is used to store the value of the counter
+ reg  clock_1,clock_0;
  
+
  always @ (posedge clock or negedge reset) 
 		begin
 			if(!reset)
-				cnt<=0;
-//			else if (cnt<(N>>1))
-         else if (cnt==N-1)
-				cnt<=0;
-			else cnt<=cnt+1; //The counter keeps counting and clears when it reaches n-1, which is a counter of modulus N
+				cnt_1<=0;
+         else if (cnt_1==N-1)
+				cnt_1<=0;
+			else cnt_1<=cnt_1+1; //The counter keeps counting and clears when it reaches n-1, which is a counter of modulus N
 		end
 always @ (posedge clock or negedge reset)
 		begin
 			if(!reset)
-				j<=0;
-			else if (cnt<(N>>1))
-				j<=0;
+				clock_1<=0;
+			else if (cnt_1<(N>>1))
+				clock_1<=0;
 			else 
-				j<=1;
+				clock_1<=1;
 		end
-assign  clock_p=j;
+		
+always @ (negedge clock or negedge reset)
+		begin
+			if(!reset)
+				cnt_0<=0;
+			else if (cnt_0==(N-1))
+				cnt_0<=0;
+			else cnt_0<=cnt_0+1;
+		end
+		 
+always @ (negedge clock or negedge reset)
+		begin
+			if(!reset)
+				clock_0<=0;
+			else if (cnt_0<(N>>1))  
+				clock_0<=0;
+			else 
+				clock_0<=1;
+		end
+
+
+assign  clock_p=clock_1&clock_0;
 
 endmodule
